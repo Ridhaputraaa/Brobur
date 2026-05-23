03c0c54214facfa0d3c73eb22c2f1fac46de8143
Ridhaputraaa <mangeak590@gmail.com>
2026-05-23 10:15:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.set_page_config(
     page_title="Brobur",
     page_icon="🧊",
-    layout="centered"
+    layout="centered",
+    initial_sidebar_state="expanded"
 )
 # Hirarki teks
 st.title("📊 Dashboard")
