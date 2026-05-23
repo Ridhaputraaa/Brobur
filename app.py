75ef5e49e3ee60e7a48dbc46928776252a1af2db
Ridhaputraaa <mangeak590@gmail.com>
2026-05-23 10:30:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,12 @@ st.caption("Made with ❤️ using Streamlit")
 
 title = st.text_input("Movie title", "Life of Brian")
 st.write("The current movie title is", title)
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
